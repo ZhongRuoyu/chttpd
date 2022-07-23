57b02a76ef6da3c7577de535fe514ee9367466d6
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-07-23 12:21:12 +0800

    Refactor

--- src/cmdline.c
+++ src/cmdline.c
@@ -16,21 +16,38 @@ void Usage(FILE *out) {
         "  -r ROOT, --root ROOT  Set root directory to serve (default: .)\n");
 }
 
+static int ReadFlagWithoutDash(int argc, char **argv, int i, size_t offset,
+                               const char *name) {
+    if (argv[i][offset] == '\0') {
+        return 0;
+    }
+    if (strcmp(argv[i] + offset, name) == 0) {
+        return 1;
+    }
+    return 0;
+}
+
 static int ReadFlag(int argc, char **argv, int i, const char *name) {
     if (argv[i][0] == '\0') {
         return 0;
     }
-    if (argv[i][0] != '-' || argv[i][1] == '\0') {
-        return 0;
+    {
+        if (argv[i][0] != '-') {
+            return 0;
+        }
+        int arg_adv = ReadFlagWithoutDash(argc, argv, i, 1, name);
+        if (arg_adv != 0) {
+            return arg_adv;
+        }
     }
-    if (strcmp(argv[i] + 1, name) == 0) {
-        return 1;
-    }
-    if (argv[i][1] != '-' || argv[i][2] == '\0') {
-        return 0;
-    }
-    if (strcmp(argv[i] + 2, name) == 0) {
-        return 1;
+    {
+        if (argv[i][1] != '-') {
+            return 0;
+        }
+        int arg_adv = ReadFlagWithoutDash(argc, argv, i, 2, name);
+        if (arg_adv != 0) {
+            return arg_adv;
+        }
     }
     return 0;
 }
@@ -40,22 +57,22 @@ static int ReadArgWithoutDash(int argc, char **argv, int i, size_t offset,
     if (argv[i][offset] == '\0') {
         return 0;
     }
-    if (strncmp(argv[i] + offset, name, strlen(name)) == 0) {
-        if (argv[i][offset + strlen(name)] == '=') {
-            *value = argv[i] + offset + strlen(name) + 1;
-            return 1;
-        }
-        if (argv[i][offset + strlen(name)] != '\0') {
-            return 0;
-        }
-        if (i + 1 >= argc) {
-            fprintf(stderr, "option %s: argument missing\n", argv[i]);
-            exit(EXIT_FAILURE);
-        }
-        *value = argv[i + 1];
-        return 2;
+    if (strncmp(argv[i] + offset, name, strlen(name)) != 0) {
+        return 0;
     }
-    return 0;
+    if (argv[i][offset + strlen(name)] == '=') {
+        *value = argv[i] + offset + strlen(name) + 1;
+        return 1;
+    }
+    if (argv[i][offset + strlen(name)] != '\0') {
+        return 0;
+    }
+    if (i + 1 >= argc) {
+        fprintf(stderr, "option %s: argument missing\n", argv[i]);
+        exit(EXIT_FAILURE);
+    }
+    *value = argv[i + 1];
+    return 2;
 }
 
 static int ReadArg(int argc, char **argv, int i, const char *name,
@@ -85,22 +102,19 @@ static int ReadArg(int argc, char **argv, int i, const char *name,
 }
 
 void ParseArguments(int argc, char **argv, Context *context) {
-    for (int i = 0, argc_adv; i < argc;) {
-        if ((argc_adv = ReadFlag(argc, argv, i, "help"))) {
+    for (int i = 0, arg_adv; i < argc;) {
+        if ((arg_adv = ReadFlag(argc, argv, i, "help"))) {
             context->help = true;
-            i += argc_adv;
-        } else if ((argc_adv = ReadArg(argc, argv, i, "h", &context->host)) ||
-                   (argc_adv =
-                        ReadArg(argc, argv, i, "host", &context->host))) {
-            i += argc_adv;
-        } else if ((argc_adv = ReadArg(argc, argv, i, "p", &context->port)) ||
-                   (argc_adv =
-                        ReadArg(argc, argv, i, "port", &context->port))) {
-            i += argc_adv;
-        } else if ((argc_adv = ReadArg(argc, argv, i, "r", &context->root)) ||
-                   (argc_adv =
-                        ReadArg(argc, argv, i, "root", &context->root))) {
-            i += argc_adv;
+            i += arg_adv;
+        } else if ((arg_adv = ReadArg(argc, argv, i, "h", &context->host)) ||
+                   (arg_adv = ReadArg(argc, argv, i, "host", &context->host))) {
+            i += arg_adv;
+        } else if ((arg_adv = ReadArg(argc, argv, i, "p", &context->port)) ||
+                   (arg_adv = ReadArg(argc, argv, i, "port", &context->port))) {
+            i += arg_adv;
+        } else if ((arg_adv = ReadArg(argc, argv, i, "r", &context->root)) ||
+                   (arg_adv = ReadArg(argc, argv, i, "root", &context->root))) {
+            i += arg_adv;
         } else {
             fprintf(stderr, "unknown command line option: %s\n", argv[i]);
             Usage(stderr);
