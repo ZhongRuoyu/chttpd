9e6f02b6d25adfaba7e9ecfd11d8179e5b6417f0
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-08-19 23:39:45 +0800

    Add distribution script

---
+++ dist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run -it --rm -v "$(pwd):/chttpd" alpine sh -c "
+apk add --no-cache build-base bash &&
+cp -r /chttpd /tmp/chttpd &&
+cd /tmp/chttpd &&
+make clean &&
+make -j \$(nproc) LDFLAGS='-static -s' &&
+version=\$(./chttpd -v | sed 's/^chttpd \(.*\) (.*)$/\1/') &&
+tar czf /chttpd/chttpd-\$version.tar.gz chttpd
+"
