6b7780cb4d703d05b8d7b4ce60585ea19420bcfb
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-09-15 21:53:18 +0800

    Simplify

--- dist/dist-linux.sh
+++ dist/dist-linux.sh
@@ -9,20 +9,7 @@ repo="$(realpath "$1")"
 arch="$2"
 version="$(sed -n 's/^VERSION = \(.*\)$/\1/p' "$repo/Makefile")"
 
-case "$arch" in
-x86_64)
-    platform=linux/amd64
-    ;;
-aarch64 | arm64)
-    platform=linux/arm64
-    ;;
-*)
-    echo "Error: unsupported architecture $arch" >&2
-    exit 1
-    ;;
-esac
-
-docker run --rm -v "$repo:/chttpd" --platform "$platform" alpine:latest sh -c "
+docker run --rm -v "$repo:/chttpd" --platform "linux/$arch" alpine:latest sh -c "
 apk add --no-cache build-base bash &&
 cp -r /chttpd /tmp/chttpd &&
 cd /tmp/chttpd &&
