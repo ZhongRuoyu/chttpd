da5b04f349280597e90e9904780e076c7f8f7d3f
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-11-10 23:47:59 +0800

    Refactor distribution scripts

--- dist/dist-linux.sh
+++ dist/dist-linux.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+set -e
+
+if [[ "$#" -ne 2 ]]; then
     echo "Usage: $(basename "$0") <repository-dir> <arch>" >&2
     exit 1
 fi
 
 repo="$(realpath "$1")"
 arch="$2"
-version="$(sed -n 's/^VERSION = \(.*\)$/\1/p' "$repo/Makefile")"
+version="$(sed -En 's/^VERSION = (.*)$/\1/p' "$repo/Makefile")"
 
 docker run --rm -v "$repo:/chttpd" --platform "linux/$arch" alpine:latest sh -c "
-apk add --no-cache gcc libc-dev make &&
-cp -r /chttpd /tmp/chttpd &&
-cd /tmp/chttpd &&
-make -B -j \"\$(nproc)\" LDFLAGS=\"-static -s\" &&
-tar czf \"/chttpd/chttpd-$version-linux-$arch.tar.gz\" chttpd
+set -e
+apk add --no-cache gcc libc-dev make
+cp -r /chttpd /tmp/chttpd
+make -C /tmp/chttpd -B -j \"\$(nproc)\" LDFLAGS=\"-static -s\"
+tar -C /tmp/chttpd -czf \"/chttpd/chttpd-$version-linux-$arch.tar.gz\" chttpd
 "
--- dist/dist-macos.sh
+++ dist/dist-macos.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <repository-dir>" >&2
+set -e
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $(basename "$0") <repository-dir>" >&2
     exit 1
 fi
 
 repo="$1"
-version="$(sed -n 's/^VERSION = \(.*\)$/\1/p' "$repo/Makefile")"
+version="$(sed -En 's/^VERSION = (.*)$/\1/p' "$repo/Makefile")"
 
 make -C "$repo" -B -j "$(sysctl -n hw.logicalcpu)" UNIVERSAL_BINARY=1
-tar -czf "chttpd-$version-macos-universal.tar.gz" "$repo/chttpd"
+tar -C "$repo" -czf "chttpd-$version-macos-universal.tar.gz" chttpd
